07c7d3a43e22649720b9dc020f1864b567dd0b4b
Yasuda Toshiyuki <toshiyuki.yasuda@icare.jpn.com>
2020-02-14 18:05:48 +0900

    00_setup完了

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -1,4 +1,33 @@
 class TryOut
+
+  attr_accessor :first_name
+  def initialize(first_name, middle_name, last_name=nil)
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
+  end
+
+  def full_name
+    name = if @last_name
+      "#{@first_name} #{@middle_name} #{@last_name}"
+    else
+      "#{@first_name} #{@middle_name}"
+    end
+    if @upcase
+      name.upcase
+    else
+      name
+    end
+  end
+
+  def upcase_full_name
+    full_name.upcase
+  end
+
+  def upcase_full_name!
+    @upcase = true
+    full_name
+  end
   # このクラスの仕様
   # コンストラクタは、2つまたは3つの引数を受け付ける。引数はそれぞれ、ファーストネーム、ミドルネーム、ラストネームの順で、ミドルネームは省略が可能。
   # full_nameメソッドを持つ。これは、ファーストネーム、ミドルネーム、ラストネームを半角スペース1つで結合した文字列を返す。ただし、ミドルネームが省略されている場合に、ファーストネームとラストネームの間には1つのスペースしか置かない
