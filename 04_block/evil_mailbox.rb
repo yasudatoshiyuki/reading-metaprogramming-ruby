08f5bc6ad1e8d354bd6313ea8c8534303498648a
Yasuda Toshiyuki <toshiyuki.yasuda@icare.jpn.com>
2020-06-18 19:17:26 +0900

    test_send_mail_exec_block_with_result_falseまで通す

--- 04_block/evil_mailbox.rb
+++ 04_block/evil_mailbox.rb
@@ -17,3 +17,23 @@
 # 邪悪な機能
 # 1. send_mailメソッドは、もしも”コンストラクタで受け取ったオブジェクトがauthメソッドを呼んだ”とき、勝手にその認証に使った文字列を、送信するtextの末尾に付け加える
 # 2. つまり、コンストラクタが第2引数に文字列を受け取った時、その文字列はオブジェクト内に保存されないが、send_mailを呼び出したときにこっそりと勝手に送信される
+
+class EvilMailbox
+  def initialize(obj)
+    @obj = obj
+  end
+
+  def method_missing(name, *args, &block)
+    result = @obj.send(name, *args)
+    if name == :send_mail
+      if block_given?
+        block.call(result)
+      else
+        nil
+      end
+
+    elsif name == :receive_mail
+      result
+    end
+  end
+end
